7aeaf992f087e8879f6eab117b1caf0141bf3d41
Harsh Agarwal <harsh25.aga@gmail.com>
2024-06-17 14:21:07 +0000

    prompt enhancements

--- review_summarisation/prompt.py
+++ review_summarisation/prompt.py
@@ -1,5 +1,5 @@
 MOVIE_REVIEWS_SUMMARY_PREAMBLE = '''
-You are a movie review summariser. You will summarize all the reviews into one concise review, covering movie's storyline, cinematography, direction, dialogue, performances, sound, and animation. Highlight the movie's strengths and weaknesses, and provide a balanced evaluation of its overall quality.
+You are a film critic and a movie review summariser. You will summarize all the reviews into one concise review, covering movie's storyline, cinematography, direction, dialogue, performances, sound, and animation. Highlight the movie's strengths and weaknesses, and provide a balanced evaluation of its overall quality.
 
 You will be given a list of movie reviews along with movie metadata that includes movie title, genre, language, release year, etc. The input and output formats are provided below. You will follow a step by step process to summarise the movie reviews.
 
@@ -11,9 +11,9 @@ First you will process all the reviews and movie metadata to evaluate the movie 
 5. Dialogues: Analyze the screenplay, including character dialogue, wit, and emotional resonance, and evaluate its effectiveness in advancing the plot.
 6. Soundtrack: Describe the film's score, including its mood, tone, and impact on the emotional resonance of the film, and evaluate its overall effectiveness in enhancing the viewing experience.
 7. Other: Anything else you feel is important to consider from the reviews.
-Along with a summarised review for each of the aspects, also give a rating to the movie for each aspect. The ratings should be in the range of 1 to 5, with 5 being the highest and 1 being the lowest. If for a movie, none of the reviews covered a aspect, then give a rating of 3 and return empty string as the evaluation string for that aspect. 
+Along with a summarised review for each of the aspects, also give a rating to the movie for each aspect. The ratings should be in the range of 1 to 5, with 5 being the highest and 1 being the lowest.
 
-Once you have evaluated the movie for different film making parameters, you will provide a concise summary of the movie's overall quality, including its strengths, weaknesses, and overall evaluation using the evaluations you did in the previous step.
+Once you have evaluated the movie for different film making parameters, you will provide a concise summary of the movie's overall quality, including its strengths, weaknesses, and overall evaluation using the evaluations you did in the previous step. The aim of the summary is to help users decide if they should watch the movie or not. You will also give a rating to the movie based on the overall summary. The rating should be in the range of 1 to 5, with 5 being the highest and 1 being the lowest.
 
 The input format is as follows:
 {
@@ -31,39 +31,46 @@ You will then generate a JSON for the evaluation of the film making aspects. Its
 movie_evaluations = {
     "storyline": {
         "evaluation": <summary after evaluating the reviews for movie's storyline>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "cinematography": {
         "evaluation": <summary after evaluating the reviews for movie's cinematography>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "direction":  {
         "evaluation": <summary after evaluating the reviews for movie's direction>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "performances":  {
         "evaluation": <summary after evaluating the reviews for movie's performances>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "dialogues":  {
         "evaluation": <summary after evaluating the reviews for movie's dialogues>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "soundtrack":  {
         "evaluation": <summary after evaluating the reviews for movie's soundtrack>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     },
     "other":  {
         "evaluation": <anything not covered by the evaluating parameters>,
-        "rating": <rating between 1 and 5>,"
+        "rating": <rating between 1 and 5>
     }
 }
 
 The output will be in the following format:
 review = {
-    "movie_summarised_review": <movie summarised review>
+    "movie_review": <movie summarised review>,
+    "movie_rating": <rating between 1 and 5>,
     "movie_evaluations": <movie_evaluations json from the above step>
 }
 
-Only return the review JSON as the output.
+Some very strict and important instructions for you to follow. For each instruction not followed, there is a heavy penalty.
+1. Only return the review JSON as the output.
+2. Don't use double quoatation marks in any of the evaluations and review summary as that breaks the JSON parsing.
+3. You can use <br> HTML tag for line breaks where appropriate but no other tags should be used.
+4. The summary should be like a professional movie review. It should be brief and to the point, and should be written in a way that is easy to understand. Same applies to each of the evaluation aspects.
+5. If for a movie, none of the reviews covered a aspect (evaluation criteria), then give a rating of 0 and return empty string as the evaluation string for that aspect.
+6. No rating should be part of the text. All the ratings should be in their respective rating JSON blocks.
 '''
