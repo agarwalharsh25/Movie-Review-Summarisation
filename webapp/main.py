38560174033582effbe759d43b003de76de660f5
Harsh Agarwal <harsh25.aga@gmail.com>
2024-06-17 14:21:32 +0000

    UI changes to movie review

--- webapp/main.py
+++ webapp/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,25 +28,27 @@ async def get_movies(request: Request):
     movie_data = []
     data_path = os.path.join(os.path.dirname(__file__), '../data')
     
-    with open(data_path+'/movies/movies.json', "r") as f:
+    with open(data_path+'/movies/movies.json', 'r') as f:
         movies = json.load(f)
     
     for movie in movies:
-        if movie['c_reviews'] == 0:
+        if movie["c_reviews"] == 0:
             continue
 
         try:
-            with open(f"{data_path}/summarised_reviews/{movie['id']}.json", "r") as f:
+            with open(f'{data_path}/summarised_reviews/{movie["id"]}.json', 'r') as f:
                 movie_review = json.load(f)
     
             movie_obj = movie
-            movie_obj["review"] = movie_review
-    
+            movie_obj["json_data"] = json.dumps({"details": movie, "review": movie_review})
+            
             movie_data.append(movie_obj)
 
         except Exception as e:
             print(e)
             continue
+
+    movie_data = sorted(movie_data, key=lambda x: datetime.strptime(x['release_date'], '%Y-%m-%d'), reverse=True)
 
     return templates.TemplateResponse("index.html", {"request": request, "movies": movie_data})
 
--- webapp/static/script.js
+++ webapp/static/script.js
@@ -1,32 +1,86 @@
+function formatDate(inputDate) {
+    const dateObj = new Date(inputDate);
+
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const formattedDate = `${monthNames[dateObj.getMonth()]} ${dateObj.getFullYear()}`;
+
+    return formattedDate;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const posters = document.querySelectorAll(".movie-poster");
     const detailsContent = document.getElementById("details-content");
     const placeholder = document.getElementById("placeholder");
-    const coverImage = document.getElementById("cover-image");
+    const coverImage = document.getElementById("movie-cover-image");
+    const posterImage = document.getElementById("movie-poster-image");
     const movieTitle = document.getElementById("movie-title");
-    const genre = document.getElementById("genre");
-    const plotSynopsis = document.getElementById("plot-synopsis");
-    const releaseDate = document.getElementById("release-date");
-    const reviewSummary = document.getElementById("review-summary");
+    const genre = document.getElementById("movie-genres");
+    const plotSynopsis = document.getElementById("movie-plot-synopsis");
+    const releaseDate = document.getElementById("movie-release-date");
+    const reviewSummary = document.getElementById("movie-review-summary");
 
     posters.forEach(poster => {
         poster.addEventListener("click", () => {
             const movie = JSON.parse(poster.dataset.movie);
-            coverImage.src = movie.cover_image;
-            movieTitle.textContent = movie.title;
-            genre.textContent = `Genre: ${movie.genre}`;
-            plotSynopsis.textContent = `Plot: ${movie.plot_synopsis}`;
-            releaseDate.textContent = `Release Date: ${movie.release_date}`;
+            coverImage.src = `https://image.tmdb.org/t/p/original${movie.details.backdrop_path}`;
+            posterImage.src = `https://image.tmdb.org/t/p/original${movie.details.poster_path}`;
+            movieTitle.textContent = movie.details.title;
+            genre.textContent = movie.details.genres.join(', ')
+            plotSynopsis.textContent = movie.details.overview;
+            releaseDate.textContent = formatDate(movie.details.release_date);
+            
+            reviewSummary.innerHTML = movie.review.movie_review;
 
-            reviewSummary.innerHTML = '';
-            for (const [aspect, summary] of Object.entries(movie.review_summary)) {
-                const li = document.createElement("li");
-                li.textContent = `${aspect.charAt(0).toUpperCase() + aspect.slice(1)}: ${summary}`;
-                reviewSummary.appendChild(li);
+            updateReviewEvaluation(movie.review.movie_evaluations, "storyline");
+            updateReviewEvaluation(movie.review.movie_evaluations, "cinematography");
+            updateReviewEvaluation(movie.review.movie_evaluations, "direction");
+            updateReviewEvaluation(movie.review.movie_evaluations, "performances");
+            updateReviewEvaluation(movie.review.movie_evaluations, "dialogues");
+            updateReviewEvaluation(movie.review.movie_evaluations, "soundtrack");
+
+            function updateReviewEvaluation(evaluationObj, evaluationMetric) {
+                const evaluationMetricDiv = document.getElementById(evaluationMetric);
+                if (evaluationObj.hasOwnProperty(evaluationMetric) && evaluationObj[evaluationMetric]["evaluation"].trim() !== '' && evaluationObj[evaluationMetric]["rating"] !== 0) {
+                    const rating = evaluationMetricDiv.querySelector('#review-evaluation-rating');
+                    const content = evaluationMetricDiv.querySelector('#review-evaluation-content');
+
+                    rating.textContent = evaluationObj[evaluationMetric]["rating"];
+                    content.innerHTML = evaluationObj[evaluationMetric]["evaluation"];
+
+                    if (evaluationMetricDiv.classList.contains("d-none")) {
+                        evaluationMetricDiv.classList.remove("d-none");
+                    }
+                } else {
+                    if (!evaluationMetricDiv.classList.contains("d-none")) {
+                        evaluationMetricDiv.classList.add("d-none");
+                    }
+                }
+            }
+            
+
+            // reviewSummary.innerHTML = '';
+            // for (const [aspect, summary] of Object.entries(movie.review_summary)) {
+            //     const li = document.createElement("li");
+            //     li.textContent = `${aspect.charAt(0).toUpperCase() + aspect.slice(1)}: ${summary}`;
+            //     reviewSummary.appendChild(li);
+            // }
+
+            if (!placeholder.classList.contains("d-none")) {
+                placeholder.classList.add("d-none");
+            }
+            if (detailsContent.classList.contains("d-none")) {
+                detailsContent.classList.remove("d-none");
             }
 
-            placeholder.classList.add("d-none");
-            detailsContent.classList.remove("d-none");
+            // Smooth scroll to the top of the page
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
         });
     });
 });
